3ba6758e8a59dcfa7bdebf171b60619562cdb2c4
Olivia Prior <olivialaineprior@gmail.com>
2018-10-02 15:00:14 -0400

    Added in video list array and videos in assets folder

---
+++ js/videoList.js
@@ -0,0 +1,10 @@
+var videoList = [
+                "bunny",
+                "hungry-kermit",
+                "lil-snail",
+                "scrapyard-pup",
+                "sharknado",
+                "sneaky-panther",
+                "very-bossy-canada-goose"
+
+]
