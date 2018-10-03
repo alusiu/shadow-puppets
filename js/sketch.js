4a40b492f733db0f379e3e87f5bd63a3b19a1154
Olivia Prior <olivialaineprior@gmail.com>
2018-10-03 14:05:12 -0400

    Added an image function to sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,25 @@
-var button, canvas, fingers, playbutton, player1, player2, playing = true, verb, welcomeGreeting, welcomeMessage;
+var button, canvas, fingers, img, playbutton, player1, player2, playing = true, verb, welcomeGreeting, welcomeMessage;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 
-  textAlign(CENTER);
-  textSize(50);
-
-  welcomeGreeting = createElement('h2', 'Welcome to puppeters', 100);
-  welcomeGreeting.position(width/2 - 50, height/5 - 50);
-
-  welcomeMessage = createElement('h4', 'To continue, please turn on the flashlight on your phone');
-  welcomeMessage.position(welcomeGreeting.x, welcomeGreeting.y + 50 );
-
-  button = createButton('I\'ve turned on my phone flashlight');
-  button.position(width/2 - 33, height/2, 65);
-  button.mousePressed(startSession);
+    textAlign(CENTER);
+    textSize(50);
+  
+    welcomeGreeting = createElement('h2', 'Welcome to puppeters', 100);
+    welcomeGreeting.position(width/2 - 50, height/5 - 50);
+  
+    welcomeMessage = createElement('h4', 'To continue, please turn on the flashlight on your phone');
+    welcomeMessage.position(welcomeGreeting.x, welcomeGreeting.y + 50 );
+  
+    button = createButton('I\'ve turned on my phone flashlight');
+    button.position(width/2 - 33, height/2, 65);
+    button.mousePressed(startSession);
   
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function draw() {
+  
 }
 
 function startSession() {
@@ -28,38 +28,38 @@ function startSession() {
   welcomeGreeting.remove();
   welcomeMessage.remove();
 
-  session(0);
+  session(1);
 
 }
 
 function session(affirm) {
 
-  console.log(affirm);
-  player1.position(canvas.x, canvas.y);
-  player1 = getGif();
-  player1video = createVideo(['../assets/'+player1+'.mov', '../assets/'+player1+'.webm']);
+  img = createImg("../assets/bird-480x480.gif");  // Load the test image
+  image(img, 0, 0);
+  
+  //player1 = getGif();
+  //player1video = createVideo(['../assets/'+player1+'.gif']);
 
-  player2 = getGif();
-  player2video = createVideo(['../assets/'+player2+'.mov', '../assets/'+player2+'.webm']);
+  // player2 = getGif();
+  // player2video = createVideo(['../assets/'+player2+'.mov', '../assets/'+player2+'.webm']);
 
   verb = getVerb();
 
-  console.log(player1);
   console.log(verb);
-  console.log(player2);
+  console.log(img);
 }
 
 function getVerb() {
+  // randomly choose a verb from the verblist and return it to the session function
+  var v = verbList[Math.floor(Math.random()*verbList.length)];
 
-  var x = verbList[Math.floor(Math.random()*verbList.length)];
-
-  return x;
+  return v;
 }
 
 function getGif() {
+  // randomly choose a gif from the gif title list and return it to the session function
+  var gifName = videoList[Math.floor(Math.random()*videoList.length)];
 
-  var y = videoList[Math.floor(Math.random()*videoList.length)];
-
-  return y;
+  return gifName;
 }
 
