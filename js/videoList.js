a3ab6c5e55909f2e13da13796b45938cea2c4a2f
LCW <laurenconnellwhitney@gmail.com>
2018-10-04 21:07:06 -0400

    Update Video list to square gifs

--- js/videoList.js
+++ js/videoList.js
@@ -1,11 +1,10 @@
 var videoList = [
-                "bird-480x580",
-                "bossy-goose-480x580",
-                "bunny-480x580",
-                "hungry-kermit-480x580",
-                "lilsnail-480x580",
-                "scrappy-pup-480x580",
-                "sharknado-480x580",
-                "sneaky-panther-480x580",
-
+                "bird-480x480",
+                "bossy-goose-480x480",
+                "bunny-480x480",
+                "hungry-kermit-480x480",
+                "lilsnail-480x480",
+                "scrappy-pup-480x480",
+                "sharknado-480x480",
+                "sneaky-panther-480x480",
 ]
